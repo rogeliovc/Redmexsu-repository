8c9ba6890655716858bf11660188efe1248f249f
rogeliovc <rv114576@gmail.com>
2025-07-10 14:51:27 -0600

    Membersdb configured for institutions

--- config.js
+++
@@ -1,5 +0,0 @@
-// Configuración de Supabase
-export const CONFIG = {
-    SUPABASE_URL: 'https://objeqynusiykuovhjauf.supabase.co',
-    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9iamVxeW51c2l5a3VvdmhqYXVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0NzIwNzQsImV4cCI6MjA2NjA0ODA3NH0.6A0jdMOLjlgE3gYsnrQb_z5ZwZScBImuIqW_yiGac1k'
-};
